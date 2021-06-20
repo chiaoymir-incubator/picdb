24d643083fbadf38d4c95463d989eabfa040ce98
Dao Jan Chang <kimnai862@gmail.com>
2021-06-20 15:58:15 +0800

    final version

--- feedback.py
+++ feedback.py
@@ -1,6 +1,7 @@
 from pymongo import MongoClient, IndexModel, TEXT
+from bson.objectid import ObjectId
 from os import listdir
-from os.path import isfile, join
+from os.path import isfile, join, isdir
 class Feedback:
 
 	def __init__(self, client, database, collection):
@@ -14,7 +15,7 @@ class Feedback:
 		result = set()
 		ptr = self.database[tag].find({})
 		for item in ptr:
-			result.add(item['img_id'])
+			result.add(item['_id'])
 		return result
 
 	def multi_tag_query(self, tags):
@@ -25,50 +26,54 @@ class Feedback:
 		return list(result)
 		
 	def feedback_folder(self, tags, filepath, positive_feedback):
-		files = [f for f in listdir(filepath) if isfile(join(filepath, f))]
-		print(files)
+		raw_files = [f for f in listdir(filepath) if isfile(join(filepath, f))]
+		
+		files = []
+		for file in raw_files:
+			item = file.split('.')[0]
+			if item != '': files += [item]
+
+		self.feedback([tags], files, positive_feedback)
+
+	def feedback_all_folder(self, filepath, positive_feedback):
+		raw_path = [f for f in listdir(filepath) if isdir(join(filepath, f))]
+		for path in raw_path:
+			self.feedback_folder(path, join(filepath, path), positive_feedback)
+
 	def feedback(self, tags, ids, positive_feedback):
 		
-		#self.database.drop_collection(tag)
 		for tag in tags:
 			if not (tag in self.database.list_collection_names()):
 				self.database.create_collection(tag)
-				print("rere")
-				index = IndexModel([('img_id', TEXT)], unique = True, dropDups=1)
-				self.database[tag].create_indexes([index])
 
 			buf = self.database[tag]
 			entity = 'credits.%s' % tag
 			value = 1 if positive_feedback else -1
-				
-			self.collection.update({'img_id' : {'$in': ids}}, {'$inc':{ entity : value}})
+			ids = [ObjectId(id) for id in ids]
+			self.collection.update_many({'_id' : {'$in': ids}}, {'$inc':{ entity : value}}, upsert=True)
 
 			if positive_feedback:
-				result = self.collection.find({'img_id' : {'$in' : ids}, entity : {'$gt' : 99}})
+				result = self.collection.find({'_id' : {'$in' : ids}, entity : {'$gt' : 99}})
 
 				delete_data, insert_data = [], []
 
 				for item in result:
-					delete_data += [item['img_id']]
-					insert_data += [{'img_id' : item['img_id']}]
+					delete_data += [item['_id']]
+					insert_data += [{'_id' : item['_id']}]
 
-				buf.delete_many({'img_id' : {'$in' : delete_data}})
-				print(delete_data)
+				if len(delete_data) == 0 : return 
+
+				buf.delete_many({'_id' : {'$in' : delete_data}})
 				buf.insert_many(insert_data)
-				tp = buf.find({})
-				for item in tp:
-					print(item)
+
 			else:
-				result = self.collection.find({'img_id' : {'$in' : ids}, entity : {'$lt' : 100}})
+				result = self.collection.find({'_id' : {'$in' : ids}, entity : {'$lt' : 100}})
 
 				delete_data = []
 
 				for item in result:
-					delete_data += [item['img_id']]
+					delete_data += [item['_id']]
 
-				buf.delete_many({'img_id' : {'$in' : delete_data}})
+				buf.delete_many({'_id' : {'$in' : delete_data}})
 
-				tp = buf.find({})
-				for item in tp:
-					print(item)
 		return
